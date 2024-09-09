229f89ec72823b79864acce423449274b49a8eeb
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-09 22:04:46 +0530

    minor change

--- script.ts
+++ script.ts
@@ -3,4 +3,5 @@ types
 types has 2 types a) primitives b) references
 a) primitives - number boolean undefined string characters
 b) references - objects classes function arrays tuples
+Typescript is nothing but JavaScript with few more features
 */
