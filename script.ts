87bfc8c4a915d273f7d6edda11716b2bb9de0401
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-10 23:05:52 +0530

    primitive and reference types example

--- script.ts
+++ script.ts
@@ -5,3 +5,16 @@ a) primitives - number boolean undefined string characters
 b) references - objects classes function arrays tuples
 Typescript is nothing but JavaScript with few more features
 */
+
+/*
+Basic Types
++ Number, String, Boolean
++ Arrays, Tuples
++ Any, Unknown, Never, Void
++ Enums
+*/
+// primitive examples
+let a : number;
+
+// array example for references types
+let arr : [];
