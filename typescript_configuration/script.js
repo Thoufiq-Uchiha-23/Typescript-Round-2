e96b84b016961a341173879b4d0dd6268270cae1
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-11 15:04:12 +0530

    compiled

---
+++ typescript_configuration/script.js
@@ -0,0 +1,29 @@
+"use strict";
+console.log("hey");
+// command to create ts.config.json file is
+// tsc --init
+// command to make save automatically and keep on watch is
+// tsc --watch
+let one = 12;
+const two = 22;
+console.log(one, two);
+let naam;
+let dabba = {
+    weight: 2,
+    color: "#FFFFFF"
+};
+function getUser(user) {
+    user.name;
+    // number type suggestion
+    user.age.toPrecision;
+}
+function abcd(human) {
+    human.name;
+    human.age;
+}
+function getMithaai(mithaai) {
+    mithaai.verySweet;
+}
+function getKhattaFood(khattafood) {
+    khattafood.verySour;
+}
