a900e526efc246abf95c9035a29abeb154d680f1
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-11 14:32:56 +0530

    compiled script from typescript-config

---
+++ dist/typescript_configuration/script.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("hey");
+// command to create ts.config.json file is
+// tsc --init
+// command to make save automatically and keep on watch is
+// tsc --watch
+let one = 12;
+const two = 22;
+console.log(one, two);
