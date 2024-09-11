dabea0b22b5901e10ad326ae3b16cc6a290d61a8
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-11 15:04:44 +0530

    Interfaces and Type Aliases

--- typescript_configuration/script.ts
+++ typescript_configuration/script.ts
@@ -7,3 +7,97 @@ let one = 12;
 const two = 22;
 
 console.log(one, two);
+
+// TYPE ALIASES
+// primitive type aliases
+type Name = string
+
+let naam: Name
+
+// object type aliases
+// type Human = {
+//     name: string,
+//     age: number,
+//     email: string,
+// }
+
+// let thoufiq: Human = {
+//     name: "Thoufiq",
+//     age: 20,
+//     email: "thoufiq@thoufiq.com"
+// }
+
+// type User = {
+//     name: string,
+//     age: number,
+//     email: string,
+//     username: string,
+// }
+
+// let user: User = {
+//     name: "Thoufiq",
+//     age: 25,
+//     email: "test@test.com",
+//     username: "test",
+// }
+
+type Dabba = {
+    weight: number,
+    color: string,
+}
+
+let dabba: Dabba = {
+    weight: 2,
+    color: "#FFFFFF"
+}
+
+// INTERFACE - this creates objects and classes shape
+interface User {
+    name: string,
+    age: number,
+    username: string,
+    email: string,
+    password: string,
+}
+
+function getUser (user:User) {
+    user.name
+    // number type suggestion
+    user.age.toPrecision
+}
+
+// Merge 2 interfaces
+interface Human {
+    name: string
+}
+
+interface Human {
+    age: number
+}
+
+function abcd(human: Human) {
+    human.name
+    human.age
+}
+
+// Interfaces with extending other properties like inheritance
+interface Food {
+    name: string,
+    price: number,
+}
+
+interface MithaFood extends Food {
+    verySweet: boolean;
+}
+
+interface KhattaFood extends Food {
+    verySour: boolean;
+}
+
+function getMithaai(mithaai:MithaFood) {
+    mithaai.verySweet
+}
+
+function getKhattaFood(khattafood:KhattaFood) {
+    khattafood.verySour
+}
