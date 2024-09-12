e708973836f927796ee49f031d461cc5d6e0bc01
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-12 21:40:20 +0530

    compiled script classes

--- dist/Classes/script.js
+++ dist/Classes/script.js
@@ -37,3 +37,40 @@ class AirConditioner {
         console.log("decreasing temperature by 1");
     }
 }
+// Example 3
+class Food {
+    constructor() {
+        this.price = 1200;
+    }
+    eat() {
+        console.log("eating");
+    }
+}
+class Mithaai extends Food {
+    constructor() {
+        super(...arguments);
+        this.name = "Mitha";
+    }
+}
+let mitha1 = new Mithaai();
+mitha1.eat;
+// Constructors
+// constructor class ka special method hota hai jiska kaam hai sab se pehle
+// chalna and saare variables jo initialize karna
+// Example 4
+class Pendrive {
+    constructor(name) {
+        this.company = name;
+    }
+}
+let p1 = new Pendrive("sandisk");
+let p2 = new Pendrive("hp");
+// Example 5
+class Earphones {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+let e1 = new Earphones("Apple", 25000);
+let e2 = new Earphones("Oppo", 2500);
