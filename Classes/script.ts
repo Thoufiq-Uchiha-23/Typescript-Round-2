21958189cdb575b1e2cf200a3a288e04acab8418
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-12 21:37:43 +0530

    learning classes with constructors

--- Classes/script.ts
+++ Classes/script.ts
@@ -15,31 +15,78 @@
 
 // Example 2
 class AirConditioner {
-    color = "white";
-    tonnes = 75
-    company = "voltas"
+  color = "white";
+  tonnes = 75;
+  company = "voltas";
 
-    temperature = 22
+  temperature = 22;
 
-    // features
-    turnOn(){
-        console.log("turning on...");
-        console.log("turned on...");
-    }
+  // features
+  turnOn() {
+    console.log("turning on...");
+    console.log("turned on...");
+  }
 
-    turnOff(){
-        console.log("turning off...");
-        console.log("turned off...");
-    }
+  turnOff() {
+    console.log("turning off...");
+    console.log("turned off...");
+  }
 
-    raiseTemperature() {
-        // console.log("temperature raised by 1");
-        this.temperature++;
-        console.log(this.temperature);
-    }
+  raiseTemperature() {
+    // console.log("temperature raised by 1");
+    this.temperature++;
+    console.log(this.temperature);
+  }
 
-    decreaseTemperature(){
-        console.log("decreasing temperature by 1");
-    }
+  decreaseTemperature() {
+    console.log("decreasing temperature by 1");
+  }
 }
+
+// Example 3
+class Food {
+  price = 1200;
+
+  eat() {
+    console.log("eating");
+  }
+}
+
+class Mithaai extends Food {
+  name = "Mitha";
+}
+
+let mitha1 = new Mithaai();
+mitha1.eat;
+
+// Constructors
+// constructor class ka special method hota hai jiska kaam hai sab se pehle
+// chalna and saare variables jo initialize karna
+// Example 4
+class Pendrive {
+  // declare/ define
+  public company: string;
+
+  constructor(name: string) {
+    this.company = name;
+  }
+}
+
+let p1 = new Pendrive("sandisk");
+let p2 = new Pendrive("hp");
+
+// Example 5
+class Earphones {
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+let e1 = new Earphones("Apple", 25000);
+let e2 = new Earphones("Oppo", 2500);
+
 
